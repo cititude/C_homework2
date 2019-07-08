dee400fb749327cf0140f8d285bbfe28edc7ec05
cititude <3223781449@qq.com>
2019-07-08 15:32:40 +0800

    fix1

--- obstacle.cpp
+++ obstacle.cpp
@@ -1,14 +1,63 @@
 #include "obstacle.h"
 #include "ui_obstacle.h"
+#include <QPixmap>
+#include <QDebug>
 
-Obstacle::Obstacle(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Obstacle)
+#define INFINITY 1000000000
+Obstacle::Obstacle(QString path,Xy_pos po,int type)
 {
-    ui->setupUi(this);
+    QPixmap pix;
+    imgpath=path;
+    pix.load(imgpath);
+    bool ret =pix.load(imgpath);
+    if(!ret)
+    {
+        qDebug()<<"fail to load!";
+        return ;
+    }
+    this->setFixedSize(20,20);
+    pix.scaled(20,20);
+    this->setStyleSheet("QLabel{border:0px;}");
+    this->setPixmap(pix);
+    pos=po;
+    this->move(pos.x*20,pos.y*20);
+    switch(type)
+    {
+        case 1:         // food
+            lifetime=INFINITY;
+            move_direction=0;
+            move_speed=0;
+            attack=0;
+            break;
+        case 2:
+
+            break;
+        default:
+            break;
+    }
 }
+
 
 Obstacle::~Obstacle()
 {
     delete ui;
 }
+
+
+void Obstacle::mymove()
+{
+
+}
+
+void Obstacle::disaapear()
+{
+
+}
+
+Xy_pos Obstacle::get_pos()
+{
+    return pos;
+}
+
+
+QList<Obstacle> obstacles;
--- obstacle.h
+++ obstacle.h
@@ -2,21 +2,36 @@
 #define OBSTACLE_H
 
 #include <QWidget>
+#include <QLabel>
+#include <board.h>
+#include <QList>
 
 namespace Ui {
 class Obstacle;
 }
 
-class Obstacle : public QWidget
+class Obstacle : public QLabel
 {
     Q_OBJECT
 
 public:
-    explicit Obstacle(QWidget *parent = nullptr);
+    explicit Obstacle(QString path,Xy_pos po,int type);
     ~Obstacle();
-
+    void mymove();
+    void disaapear();
+    Xy_pos get_pos();
 private:
     Ui::Obstacle *ui;
+    Xy_pos pos;
+    int lifetime;
+    int move_direction;
+    int move_speed;
+    int type;           // 1:food
+    bool can_pass;
+    int attack;
+    QString imgpath;
 };
 
 #endif // OBSTACLE_H
+
+extern QList<Obstacle> obstacles;
